940bdaade82ec96cd98c5a3ed84a64c0fbef8778
Yuit Xin <yuitxin@gmail.com>
2025-12-01 22:19:53 +0800

    homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,21 +1,37 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import '../App.css';
+import {useEffect, useState} from 'react';
 
 export default function Homepage() {
   const navigate = useNavigate();
+  const [name,setName] = useState('');
+  useEffect(() => {
+    const storedName = localStorage.getItem('name');
+    setName(storedName);}, 
+    []);
 
   return (
     <div className="gradient-bg">
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>Welcome to Super Stressed App</h1>
+      <div className="home-tittle">
+        <h1>Super Stressed App</h1>
+        <h2>Welcome,{name}</h2>
         <p>A safe space to share your thoughts and feelings.</p>
+        
+          <button onClick={() => navigate("/diary")}>
+            Digital Diary
+            <span className="button-subtext">Write your thoughts and feelings here.</span>
+          </button>
+            
+          <button onClick={() => navigate("/mood")}>
+            Mood Tracker
+            <span className="button-subtext">Track your emotional wellbeing.</span>
+          </button>
 
-        <div style={{ display: "flex", justifyContent: "center", gap: "20px", marginTop: "20px" }}>
-          <button onClick={() => navigate("/diary")}>Digital Diary</button>
-          <button onClick={() => navigate("/mood")}>Mood Tracker</button>
-          <button onClick={() => navigate("/pet")}>Pet Adoption</button>
-        </div>
+          <button onClick={() => navigate("/pet")}>
+            Pet Adoption
+            <span className="button-subtext">Care for your visual companion.</span>
+          </button>
       </div>
     </div>
   );
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import '../App.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function Login() {
     const navigate = useNavigate();
+    const [name,setName] = useState('');
+
+    const handlesubmit = () => {
+        localStorage.setItem('name', name);
+        navigate('/homepage');
+    }
 
     return (
         <div className="gradient-bg">
@@ -13,9 +20,9 @@ export default function Login() {
 
                 <div className="input-group">
                     <p>Enter your name</p>
-                    <input type="text" placeholder="Username" />
+                    <input type="text" placeholder="Username" value={name} onChange={(e) => setName(e.target.value)}/>
                 </div>
-                <button onClick = { () => navigate ('/homepage')}> Get started!</button>
+                <button onClick = {handlesubmit}> Get started!</button>
             </div>
 
         </div>
